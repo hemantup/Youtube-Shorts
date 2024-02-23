367bc508877ad2c2f7a70db90f4813b363570cf4
Hemant Upadhyay <hemantupadhyay3012002@gmail.com>
2024-02-23 22:17:08 +0530

    first commit

---
+++ app/src/main/java/com/upadhyay/hemant/youtubeshorts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.upadhyay.hemant.youtubeshorts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
